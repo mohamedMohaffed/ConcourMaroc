f08e36e4b189feeadf9e9982a687eff4a98c05b9
mohamed <cd445325@gmail.com>
2025-08-27 15:17:46 +0100

    add base of user anser

--- backend/concour/serializers.py
+++ backend/concour/serializers.py
@@ -84,4 +84,5 @@ class ConcourSerializer(serializers.ModelSerializer):
             }
         ).data
 
-    
+
+
---
+++ backend/concour/views_score.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import (UserAnswer)
+from .serializers import ()
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+
+class UserAnswerAPIView(APIView):
+    def post(request):
+        serializer = 11
+        
+
